935e0ec649ee892bd3844ff4c292931da1378752
RafaStutz <rafaelstutz14@gmail.com>
2026-01-06 09:59:05 -0300

    add hashmap

---
+++ data_structures/hashmap.py
@@ -0,0 +1,42 @@
+class HashMap():
+    def __init__(self, size = 100) -> None:
+        self.size = size
+        self.buckets = [[] for _ in range(size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def put(self, key, value):
+        index = delf._hash(key)
+        bucket = self.buckets[index]
+
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, value)
+                return
+        
+        bucket.append((key, value))
+    
+    def get(self, key):
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for k, v in bucket:
+            if k == key:
+                return value
+            
+        return None
+
+    def remove(self, key):
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                del bucket[i]
+                return
+    
+
+## TODO: add resizing, load factor, and use linked-lists.
+
+

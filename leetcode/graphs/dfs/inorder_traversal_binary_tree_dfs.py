8ff4bbaddf319bff967e490249bf7dd825542235
RafaStutz <rafaelstutz14@gmail.com>
2026-03-04 11:45:36 -0300

    inorder traversal problem

---
+++ leetcode/graphs/dfs/inorder_traversal_binary_tree_dfs.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        res = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+            
+        return res
+        
+# Time Complexity: O(n) where n is the number of nodes in the binary tree, as we visit each node once.
+# Space Complexity:  O(n) in the worst case.
